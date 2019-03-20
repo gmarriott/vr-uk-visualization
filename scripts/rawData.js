43f56c59c0fb147bc79e6057e373395ef7dfc16d
George Marriott <marriottg94@hotmail.co.uk>
2019-03-20 22:09:15 +0000

    Initial commit

---
+++ scripts/rawData.js
@@ -0,0 +1,24 @@
+var rawData = [
+{x:6.36, y:0, z:5.37, height:3.76, color: '#2b8c26'},
+{x:6.36, y:0, z:5.44, height:2.89, color: '#4f20c7'},
+{x:6.55, y:0, z:5.20, height:4.11, color: '#9A2529'},
+{x:4.70, y:0, z:2.75, height:3.07, color: '#2b8c26'},
+{x:4.61, y:0, z:2.77, height:3.44, color: '#4f20c7'},
+{x:4.53, y:0, z:2.70, height:3.78, color: '#9A2529'},
+{x:2.47, y:0, z:2.16, height:2.15, color: '#2b8c26'},
+{x:2.33, y:0, z:2.21, height:2.32, color: '#4f20c7'},
+{x:2.37, y:0, z:2.16, height:3.51, color: '#9A2529'},
+
+];
+
+var chartData=[
+{name: "London - C#", color:"#2b8c26", year: 2007, value: 54},
+{name: "London - React",color:"#4f20c7", year: 2008, value: 44},
+{name: "London - PHP",color:"#9A2529", year: 2009, value: 65},
+{name: "Manchester - C#",color:"#2b8c26", year: 2010, value: 54},
+{name: "Manchester - React", color:"#4f20c7",year: 2011, value: 64},
+{name: "Manchester - PHP", color:"#9A2529",year: 2012, value: 44},
+{name: "Sheffield - C#",color:"#2b8c26", year: 2013, value: 43},
+{name: "Sheffield - React",color:"#4f20c7", year: 2014, value: 77},
+{name: "Sheffield - PHP",color:"#9A2529",year: 2015, value: 66}
+];
